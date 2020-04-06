7a5d68eee756094f4c7c4ff8064d1ffed1a5d013
Vanina Quezada <vaninaquezada@gmail.com>
2020-04-05 23:57:24 -0300

    Tp juegos se se puede ejecutar

--- src/app/servicios/mi-http/mi-http.service.ts
+++ src/app/servicios/mi-http/mi-http.service.ts
@@ -1,12 +1,14 @@
+
+import {throwError as observableThrowError,  Observable } from 'rxjs';
+
+import {catchError, map} from 'rxjs/operators';
 import { log } from 'util';
 import { Injectable } from '@angular/core';
 
 import { Http, Response } from '@angular/http';
 
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/toPromise';
+
+
 
 @Injectable()
 export class MiHttpService {
@@ -34,9 +36,9 @@ export class MiHttpService {
 
   public httpGetO ( url: string): Observable<Response>
   {
-    return this.http.get( url )
-      .map( ( res: Response ) => res.json())
-      .catch( ( err: any ) => Observable.throw(err.json().error || 'Server error'));
+    return this.http.get( url ).pipe(
+      map( ( res: Response ) => res.json()),
+      catchError( ( err: any ) => observableThrowError(err.json().error || 'Server error')),);
   }
 
 
